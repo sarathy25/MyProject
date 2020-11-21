ad9095aa4149c1698b4b91110a5da7f1bd525920
sarathy2511 <sarathy2511@gmail.com>
2020-11-21 07:51:30 +0530

    my project

---
+++ src/org/test/Employee.java
@@ -0,0 +1,18 @@
+package org.test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Employee {
+public static void main(String[] args) {
+	
+	String[]n = new String[] {"partha","sarathy"};
+	List<String> list = Arrays.asList(n);
+	System.out.println(list);
+   
+ }  
+
+}
